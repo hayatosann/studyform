40728260b570f661c93ff96a4a629f79a7ce1cc3
hayatosann <sasano.hayato@icloud.com>
2019-10-05 19:47:46 +0900

    Set controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.save!
+    redirect_to article_path(params[:article_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(article_id: params[:article_id])
+  end
 end
