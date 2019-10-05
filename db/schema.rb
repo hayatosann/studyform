d10e5123e04363a81a8b44e553946f1ed6d6ec34
hayatosann <sasano.hayato@icloud.com>
2019-10-05 20:54:14 +0900

    Add column in devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_080612) do
+ActiveRecord::Schema.define(version: 2019_10_05_112702) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "study_time"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2019_10_05_080612) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
