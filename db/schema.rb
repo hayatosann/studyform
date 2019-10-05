bad12a2f70b6d13135d28cf2f04137387d645a6b
hayatosann <sasano.hayato@icloud.com>
2019-10-05 16:40:28 +0900

    Make model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_071459) do
+ActiveRecord::Schema.define(version: 2019_10_05_073558) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "study_time"
+    t.string "study_field"
+    t.text "study_summary"
+    t.text "itnews_opinion"
+    t.string "consultation"
+    t.string "sns_links"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
