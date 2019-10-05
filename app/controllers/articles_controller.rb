31f1c6df0796100de5a87d052295940e804f3eff
hayatosann <sasano.hayato@icloud.com>
2019-10-05 18:47:13 +0900

    process saving function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,16 @@ class ArticlesController < ApplicationController
   def new
     @ariticle = Article.new 
   end
+
+  def create
+    binding.pry
+    @article = current_user.articles.new(article_params)
+    @article.save!
+  end
+
+  private
+  
+  def article_params
+    params.permit(:study_time, :study_field, :study_summary, :itnews_opinion, :consultation, :sns_links)
+  end
 end
